ed3df41d8c9584442377ef5890231cdb673915de
wellington <wellmil.live@gmail.com>
2024-09-29 17:05:37 -0300

    Seção 7 Tópicos - Operações com bits

---
+++ src/sec7_bitwise/OperarBit.java
@@ -0,0 +1,15 @@
+package sec7_bitwise;
+
+public class OperarBit {
+
+	public static void main(String[] args) {
+		int x = 89;
+		int y = 60;
+		
+		System.out.println(x & y);
+		System.out.println(x | y);
+		System.out.println(x ^ y);
+
+	}
+
+}
